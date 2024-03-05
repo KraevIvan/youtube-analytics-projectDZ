b2c8586624b72fe9c286facd47f9fc524cef5ed2
KraevIvan <KraevIvanA@yandex.ru>
2024-03-05 10:42:42 +0300

    add new attributes and new methods(get_service and to_json)

--- src/channel.py
+++ src/channel.py
@@ -13,7 +13,20 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = "https://www.youtube.com/channel/" + self.channel_id
+        self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -22,3 +35,16 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         printj(channel)
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        channel = build('youtube', 'v3', developerKey=api_key)
+        return channel
+
+    def to_json(self,name_file):
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        with open(name_file, 'w') as file:
+            file.write(json.dumps(channel))
