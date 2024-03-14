9ebf144350596b1b364327aa979f55b95f6ad5d5
KraevIvan <KraevIvanA@yandex.ru>
2024-03-14 23:18:10 +0300

    fix video

--- src/video.py
+++ src/video.py
@@ -1,9 +1,7 @@
-import json
 import os
 
 from googleapiclient.discovery import build
 
-import isodate
 
 api_key: str = os.getenv('YOUTUBE_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
