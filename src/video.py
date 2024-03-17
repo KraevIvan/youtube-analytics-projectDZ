7a6cdddcb12e071ca939abe25c0b4f0375fa426c
KraevIvan <KraevIvanA@yandex.ru>
2024-03-17 22:02:02 +0300

    add broken id error

--- src/video.py
+++ src/video.py
@@ -10,15 +10,24 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Video:
 
     def __init__(self, video_id):
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.id = video_id
-        self.title = video_response['items'][0]['snippet']['title']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count = video_response['items'][0]['statistics']['commentCount']
-        self.url = "https://youtu.be/" + video_id
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.id = video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count = video_response['items'][0]['statistics']['commentCount']
+            self.url = "https://youtu.be/" + video_id
+        except LookupError:
+            print(f"Неверное id видео!")
+            self.id = video_id
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.url = None
 
     def __str__(self):
         return f"{self.title}"
